673e146eb452ee5bf55719823925c4751764256c
isaccanedo <isaccanedo@gmail.com>
2021-08-02 10:48:07 -0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os/exec"
+	"strings"
+)
+
+func asyncLog(reader io.ReadCloser) error {
+	bucket := make([]byte, 1024)
+	buffer := make([]byte, 100)
+	for {
+		num, err := reader.Read(buffer)
+		if err != nil {
+			if err == io.EOF || strings.Contains(err.Error(), "closed") {
+				err = nil
+			}
+			return err
+		}
+		if num > 0 {
+			line := ""
+			bucket = append(bucket, buffer[:num]...)
+			tmp := string(bucket)
+			if strings.Contains(tmp, "\n") {
+				ts := strings.Split(tmp, "\n")
+				if len(ts) > 1 {
+					line = strings.Join(ts[:len(ts)-1], "\n")
+					bucket = []byte(ts[len(ts)-1]) //Se não for suficiente para toda a linha, trataremos disso mais tarde
+				} else {
+					line = ts[0]
+					bucket = bucket[:0]
+				}
+				fmt.Printf("%s\n", line)
+			}
+
+		}
+	}
+	return nil
+}
+
+func execute() error {
+	cmd := exec.Command("sh", "-c", "./scripts/curl.sh")
+
+	stdout, _ := cmd.StdoutPipe()
+	stderr, _ := cmd.StderrPipe()
+
+	if err := cmd.Start(); err != nil {
+		log.Printf("Error starting command: %s......", err.Error())
+		return err
+	}
+
+	go asyncLog(stdout)
+	go asyncLog(stderr)
+
+	if err := cmd.Wait(); err != nil {
+		log.Printf("Error waiting for command execution: %s......", err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func main(){
+	execute()
+}
